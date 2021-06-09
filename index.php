9ce0b58f06c26e4838dad68be8d8fcffedd9f1e6
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 13:18:59 +0200

    creation

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex2.7 condition</title>
+    </head>
+    <body >
+        <?php
+        $isOk=TRUE;
+        if( $isOk == FALSE)
+        {
+            echo ("C'est pas bon !!!");
+        }
+        else
+        {
+            echo ("C'est OK");
+        }
+       ?>
+    </body>
+</html>
